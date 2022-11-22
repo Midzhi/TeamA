832c366573edbcf31e84637dee1b8776211567c4
miss_golden <you@example.com>
2022-11-22 19:28:09 +0600

    DreamTeam

---
+++ Modul1/Modul1 - Task5.py
@@ -0,0 +1,19 @@
+
+# Задание №5:
+# Какое слово нужно сложить 5 раз чтобы получить число 5?
+# Какое слово нужно умножить на 7 чтобы получить 7?
+
+name = ['abdrey','alex' ,'oly','jon','djek']
+print(str(len(name)))
+
+s= input('enter the number')
+print(len(s))
+print(s*1)
+
+name = input('enter the number')
+print(len(name))
+print(name+'1')
+
+
+
+
